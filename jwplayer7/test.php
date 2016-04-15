0c2f83f78909660cedd7a5234dfe92242e9ec216
Imran Rizvi <imranrizvi.xpmedia@nxvt.com>
2016-04-15 21:04:41 +0500

    play video vai jwplayer

---
+++ jwplayer7/test.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Jwplayer Video</title>
+        <script type="text/javascript" src="jwplayer.js"></script>
+        <script type="text/javascript">
+            jwplayer.key = "jmLANMTkutjrLkTBSjg0Uqg5YSuawFb9C5RWNw==";
+        </script>
+
+    </head>
+
+    <body>
+        <div id="myVideo"></div>
+    </body>
+    <script>
+        var playerInstance = jwplayer("myVideo");
+        playerInstance.setup({
+            file : "https://player.vimeo.com/external/162914398.sd.mp4?s=979f2a78030b27e9bf8e50b7225ee4fa3195893d&profile_id=164",
+        });
+    </script>
+</html>
